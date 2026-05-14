dc48a81f9cff1734ada3d06be99407504622f1e0
AnendaD <erddorz@gmail.com>
2026-05-15 00:52:47 +0300

    fixed script bug

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "Running migrations..."
-/app/migrate -path=/app/migrations -datavase="$DATABASE_URL" up
+/app/migrate -path=/app/migrations -database="$DATABASE_URL" up
 
 echo "Starting app..."
 exec /app/app
